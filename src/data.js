3be8887e2842602fcf4b5f66c4c6e94390079eac
sourabhpatole <sourabhptole@outlook.com>
2022-04-17 22:20:37 +0530

    final deploy

--- src/data.js
+++ src/data.js
@@ -1,24 +1,24 @@
 export const products = [
   {
     id: 1,
-    img: "https://ik.imagekit.io/gfznj5ukgkq/blog_1KHW0LT5s.png?ik-sdk-version=javascript-1.4.3&updatedAt=1647599877450",
-    link: "https://github.com/sourabhpatole/blog",
-    netlify: "https://simpleblogpro.herokuapp.com/",
-    name: "MERN Blog",
-  },
-  {
-    id: 2,
     img: "https://res.cloudinary.com/dt0wio0tq/image/upload/v1649057008/simpleshop_pmudst.png",
     link: "https://github.com/sourabhpatole/shop",
     netlify: "https://simpleshoppro.herokuapp.com/",
     name: "MERN Simple Store",
   },
   {
-    id: 3,
+    id: 2,
     img: "https://res.cloudinary.com/dt0wio0tq/image/upload/v1649855395/messenger_eqdbo8.png",
     link: "https://github.com/sourabhpatole/chat-app",
     netlify: "https://simplemessengerpro.herokuapp.com/",
     name: "MERN Messenger",
+  },
+  {
+    id: 3,
+    img: "https://ik.imagekit.io/gfznj5ukgkq/blog_1KHW0LT5s.png?ik-sdk-version=javascript-1.4.3&updatedAt=1647599877450",
+    link: "https://github.com/sourabhpatole/blog",
+    netlify: "https://simpleblogpro.herokuapp.com/",
+    name: "MERN Blog",
   },
   {
     id: 4,
